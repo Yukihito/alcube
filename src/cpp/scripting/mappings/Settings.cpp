8bfab533d2e5566ac01033f7884468a25402d3d5
Yukihito <yukihito.ho@gmail.com>
2018-04-22 09:06:01 +0900

    wip

--- src/cpp/scripting/mappings/Settings.cpp
+++ src/cpp/scripting/mappings/Settings.cpp
@@ -2,14 +2,11 @@
 
 namespace alcube::scripting::mappings {
   using namespace utils;
-  //PhysicsSettings::PhysicsSettings(alcube::models::PhysicsSettings *underlying) : SingletonPrototype(underlying) {}
   void PhysicsSettings::init() {
     Prototype::init();
     define<float, timeStepSize>();
     define<float, gravity>();
   }
-
-  //WindowSettings::WindowSettings(alcube::models::WindowSettings *underlying) : SingletonPrototype(underlying) {}
 
   void WindowSettings::init() {
     Prototype::init();
@@ -17,15 +14,11 @@ namespace alcube::scripting::mappings {
     define<int, height>();
   }
 
-  //WorldSettings::WorldSettings(alcube::models::WorldSettings *underlying) : SingletonPrototype(underlying) {}
-
   void WorldSettings::init() {
     Prototype::init();
     define<float, size>();
     define<int, maxActorCount>();
   }
-
-  //Settings::Settings(alcube::models::Settings *underlying) : SingletonPrototype(underlying) {}
 
   void Settings::init() {
     Prototype::init();
